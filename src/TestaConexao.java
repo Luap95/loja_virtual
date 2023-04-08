f2448f576cdf04c7ebb8624be4f36391b173db26
Lucas <lucas.7795.la@gmail.com>
2023-04-08 17:59:44 -0300

    Renomeando classe Main para TestaConnexao

--- src/TestaConexao.java
+++ src/TestaConexao.java
@@ -2,7 +2,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class Main {
+public class TestaConexao {
     public static void main(String[] args) throws SQLException {
         Connection connection = DriverManager
                 .getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC",
