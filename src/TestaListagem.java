ba4d73ed270fd50fe82748ab14508ee864c82d60
Lucas <lucas.7795.la@gmail.com>
2023-04-09 10:54:12 -0300

    Adicionando comentários

--- src/TestaListagem.java
+++ src/TestaListagem.java
@@ -3,15 +3,16 @@ import java.sql.*;
 public class TestaListagem {
 
     public static void main(String[] args) throws SQLException {
+        //Abrindo conexão ao banco de dados
         Connection connection = DriverManager
                 .getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC",
                         "root", "bd@Lucas95");
-
+        //Criando e executando Statement para consulta do banco de dados
         Statement statement = connection.createStatement();
         statement.execute("SELECT * FROM PRODUTO");
-
+        //Recebendo o resultado da consulta
         ResultSet resultSet = statement.getResultSet();
-
+        //Percorrendo e imprimindo todos os registros da consulta
         while (resultSet.next()){
             Integer id = resultSet.getInt("ID");
             System.out.println(id);
@@ -22,7 +23,7 @@ public class TestaListagem {
         }
 
         System.out.println("Fechando a conexão!!!");
-
+        //Fechando a conexão
         connection.close();
     }
 }
