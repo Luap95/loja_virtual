69b723e082b57aca7fadbf3168f0504e4bbd9750
Lucas <lucas.7795.la@gmail.com>
2023-04-11 21:58:09 -0300

    Fazendo inserções utilizando PreparedStatement

---
+++ src/TestaInsercaoComParametros.java
@@ -0,0 +1,32 @@
+import java.sql.*;
+
+public class TestaInsercaoComParametros {
+    public static void main(String[] args) throws SQLException {
+        //Variaveis com dados que serão inseridos
+        String nome = "Mouse";
+        String descricao = "Mouse sem fio";
+
+        //Recuperando uma conexão ao banco de dados
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        Connection connection = connectionFactory.RecuperaConexao();
+
+        //Criando um PreparedStatement e definindo quais são os parâmetros com "?"
+        PreparedStatement statement = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) " +
+                        "VALUES (? , ?)", Statement.RETURN_GENERATED_KEYS);
+
+        //Definindo quais serão dados que representarão o parâmetros
+        statement.setString(1, nome);
+        statement.setString(2, descricao);
+
+        //Execução retorna chave
+        statement.execute();
+
+        //Atribuindo a chave retornada ao ResultSet
+        ResultSet resultSet = statement.getGeneratedKeys();
+        //Listando as chaves criadas
+        while (resultSet.next()){
+            Integer id = resultSet.getInt(1);
+            System.out.println("O id criado para o produto é " + id);
+        }
+    }
+}
