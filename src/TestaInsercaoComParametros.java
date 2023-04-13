10404629b0480f59fbb61131b9d881642f8f8354
Lucas <lucas.7795.la@gmail.com>
2023-04-13 19:51:24 -0300

    Utilizando commit e rollback para controlar as transações do banco de dados

--- src/TestaInsercaoComParametros.java
+++ src/TestaInsercaoComParametros.java
@@ -2,18 +2,35 @@ import java.sql.*;
 
 public class TestaInsercaoComParametros {
     public static void main(String[] args) throws SQLException {
-        //Variaveis com dados que serão inseridos
-        String nome = "Mouse";
-        String descricao = "Mouse sem fio";
 
         //Recuperando uma conexão ao banco de dados
         ConnectionFactory connectionFactory = new ConnectionFactory();
         Connection connection = connectionFactory.RecuperaConexao();
+        //Tirando o poder de commit do JDBC
+        connection.setAutoCommit(false);
 
-        //Criando um PreparedStatement e definindo quais são os parâmetros com "?"
-        PreparedStatement statement = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) " +
-                        "VALUES (? , ?)", Statement.RETURN_GENERATED_KEYS);
+        try{
+            //Criando um PreparedStatement e definindo quais são os parâmetros com "?"
+            PreparedStatement statement = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) " +
+                    "VALUES (? , ?)", Statement.RETURN_GENERATED_KEYS);
 
+            adicionaVariavel("SmartTV", "45 polegadas", statement);
+            adicionaVariavel("Radio", "Radio de bateria", statement);
+
+            //Confimando as operações pendentes
+            connection.commit();
+        } catch (Exception e){
+            //Imprimindo o tipo de erro
+            e.printStackTrace();
+            System.out.println("ROLLBACK EXECUTADO");
+            //Executando o rollback
+            connection.rollback();
+        }
+        //Fechando o banco de dados
+        connection.close();
+    }
+
+    private static void adicionaVariavel(String nome, String descricao, PreparedStatement statement) throws SQLException {
         //Definindo quais serão dados que representarão o parâmetros
         statement.setString(1, nome);
         statement.setString(2, descricao);
@@ -28,8 +45,6 @@ public class TestaInsercaoComParametros {
             Integer id = resultSet.getInt(1);
             System.out.println("O id criado para o produto é " + id);
         }
-        System.out.println("Fechando a conexão!!!");
-        //Fechando a conexão
-        connection.close();
+        resultSet.close();
     }
 }
